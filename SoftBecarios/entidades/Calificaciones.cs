eb9bde8a9b489db957feb647c5aa235610d2fe89
Efrain Jazo <jage_82@hotmail.com>
2016-08-08 14:02:09 -0600

    Se crearon dos clases en Entidades una Expedientes y otra Calificaciones para los metodos GET y SET. | 08 de Agosto del 2016.

---
+++ SoftBecarios/entidades/Calificaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calificaciones
+    {
+        private int bimestre;
+        private int cal_cogno;
+        private int cal_psico;
+        private int cal_afect;
+        private int cal_final;
+        private int id_alumno;
+        private int id_servicio;
+
+        public int Bimestre { get { return bimestre; } set { bimestre = value; } }
+        public int Cal_Cogno { get { return cal_cogno; } set { cal_cogno = value; } }
+        public int Cal_Psico { get { return cal_psico; } set { cal_psico = value; } }
+        public int Cal_Afect { get { return cal_afect; } set { cal_afect = value; } }
+        public int Cal_Final { get { return cal_final; } set { cal_final = value; } }
+        public int ID_Alumno { get { return id_alumno; } set { id_alumno = value; } }
+        public int ID_Servicio { get { return id_servicio; } set { id_servicio = value; } }
+    }
+}
---
+++ SoftBecarios/entidades/Expediente.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Expediente
+    {
+        private int tipo;
+        private int numero;
+        private string nombre;
+        private string apellidop;
+        private string apellidom;
+        private int genero;
+        private string nacimiento;
+        private string lugar;
+        private int civil;
+        private int escuela;
+        private string rfc;
+        private string curp;
+        private int religion;
+        private string fec_inicio;
+        private string fec_termino;
+        private string telefono;
+        private string mail;
+        private string domicilio;
+        private string colonia;
+        private int guardia;
+        private string cedula;
+        private int servicio;
+        private int turno;
+        private double t_pantalon;
+        private double t_filipina;
+        private double t_bata;
+        private double t_zapato;
+        private string per_referencia;
+        private string tel_referencia;
+        private int tipo_sangre;
+        private string alergias;
+        private string enf_cronica;
+        private string med_cronico;
+        private string vac1_inicio;
+        private string vac1_termino;
+        private string vac2_inicio;
+        private string vac2_termino;
+        private double promedio;
+
+        public int Tipo { get { return tipo; } set { tipo = value; } }
+        public int Numero { get { return numero; } set { numero = value; } }
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string ApellidoP { get { return apellidop; } set { apellidop = value; } }
+        public string ApellidoM { get { return apellidom; } set { apellidom = value; } }
+        public int Genero { get { return genero; } set { genero = value; } }
+        public string Nacimiento { get { return nacimiento; } set { nacimiento = value; } }
+        public string Lugar { get { return lugar; } set { lugar = value; } }
+        public int Civil { get { return civil; } set { civil = value; } }
+        public int Escuela { get { return escuela; } set { escuela = value; } }
+        public string RFC { get { return rfc; } set { rfc = value; } }
+        public string CURP { get { return curp; } set { curp = value; } }
+        public int Religion { get { return religion; } set { religion = value; } }
+        public string Fec_Inicio { get { return fec_inicio; } set { fec_inicio = value; } }
+        public string Fec_Termino { get { return fec_termino; } set { fec_termino = value; } }
+        public string Telefono { get { return telefono; } set { telefono = value; } }
+        public string Mail { get { return mail; } set { mail = value; } }
+        public string Domicilio { get { return domicilio; } set { domicilio = value; } }
+        public string Colonia { get { return colonia; } set { colonia = value; } }
+        public int Guardia { get { return guardia; } set { guardia = value; } }
+        public string Cedula { get { return cedula; } set { cedula = value; } }
+        public int Servicio { get { return servicio; } set { servicio = value; } }
+        public int Turno { get { return turno; } set { turno = value; } }
+        public double T_Pantalon { get { return t_pantalon; } set { t_pantalon = value; } }
+        public double T_Filipina { get { return t_filipina; } set { t_filipina = value; } }
+        public double T_Bata { get { return t_bata; } set { t_bata = value; } }
+        public double T_Zapato { get { return t_zapato; } set { t_zapato = value; } }
+        public string Per_Referencia { get { return per_referencia; } set { per_referencia = value; } }
+        public string Tel_Referencia { get { return tel_referencia; } set { tel_referencia = value; } }
+        public int Tipo_Sangre { get { return tipo_sangre; } set { tipo_sangre = value; } }
+        public string Alergias { get { return alergias; } set { alergias = value; } }
+        public string Enf_Cronica { get { return enf_cronica; } set { enf_cronica = value; } }
+        public string Med_Cronico { get { return med_cronico; } set { med_cronico = value; } }
+        public string Vac1_Inicio { get { return vac1_inicio; } set { vac1_inicio = value; } }
+        public string Vac1_Termino { get { return vac1_termino; } set { vac1_termino = value; } }
+        public string Vac2_Inicio { get { return vac2_inicio; } set { vac2_inicio = value; } }
+        public string vac2_Termino { get { return vac2_termino; } set { vac2_termino = value; } }
+        public double Promedio { get { return promedio; } set { promedio = value; } }
+
+
+    }
+}
